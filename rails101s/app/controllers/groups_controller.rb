38fefc013b069d9003c84bdad597d93969fb4499
jrubyuncle <jrubyuncle@gmail.com>
2015-10-16 14:48:18 +0800

    3.6 in application_helper.rb, add method notice_message()        in application.html.erb, <%= notice_message %>        test: in groups#index, flash[:notice] = "message"

--- rails101s/app/controllers/groups_controller.rb
+++ rails101s/app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
   def index
+    flash[:notice] = "(notice) good morning"
+    flash[:alert] = "this is alert"
+    flash[:warning] = "this is warning"
   end
 end

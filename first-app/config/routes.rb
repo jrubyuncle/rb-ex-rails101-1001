be1383a6ba668cfb5064c29a531493fbb3f7e1dc
jrubyuncle <jrubyuncle@gmail.com>
2015-10-16 12:18:14 +0800

    1.1.6 set root route as 'topics#index'

--- first-app/config/routes.rb
+++ first-app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'topics#index'
   get 'welcome', to: 'topics#welcome'
   resources :topics
   # The priority is based upon order of creation: first created -> highest priority.

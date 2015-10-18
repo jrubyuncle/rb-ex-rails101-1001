97e4ca457fb58ee926c86473840f632314cb5ffd
jrubyuncle <jrubyuncle@gmail.com>
2015-10-18 21:36:00 +0800

    6.4.1 only author can edit/destroy group

--- rails101s/app/controllers/groups_controller.rb
+++ rails101s/app/controllers/groups_controller.rb
@@ -18,7 +18,7 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(group_params)
+    @group = current_user.groups.new(group_params)
 
     if @group.save
       redirect_to groups_path
@@ -28,11 +28,11 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find params[:id]
+    @group = current_user.groups.find params[:id]
   end
 
   def update
-    @group = Group.find params[:id]
+    @group = current_user.groups.find params[:id]
 
     if @group.update group_params
       redirect_to groups_path, notice: "modify ok"
@@ -42,7 +42,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find params[:id]
+    @group = current_user.groups.find params[:id]
     @group.destroy
     redirect_to groups_path, notice: "destroy group ok"
   end

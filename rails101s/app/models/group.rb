99ee967fd92d49f564247bcf7a4f8baba4ab9c23
jrubyuncle <jrubyuncle@gmail.com>
2015-10-19 12:44:03 +0800

    7.2 create join table: group_users

--- rails101s/app/models/group.rb
+++ rails101s/app/models/group.rb
@@ -6,6 +6,9 @@ class Group < ActiveRecord::Base
   belongs_to :owner, class_name: "User", foreign_key: :user_id
   #belongs_to :user
 
+  has_many :group_users
+  has_many :members, through: :group_users, source: :user
+  
   def editable_by_user?(user)
       user == owner
   end

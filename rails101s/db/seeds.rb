3c52ea77fe0c7346cf56afc9150413d26863e628
jrubyuncle <jrubyuncle@gmail.com>
2015-10-28 17:05:35 +0800

    10.3 seed.rb: create user, group and post

--- rails101s/db/seeds.rb
+++ rails101s/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#User.create email: "e1@gmail.com", name: "e1", password: "12345678", password_confirmation: "12345678"
+#User.create email: "e2@gmail.com", name: "e1", password: "12345678", password_confirmation: "12345678"
+u = User.create email: "example@gmail.com", name: "for-test", password: "12345678", password_confirmation: "12345678"
+
+20.times do |i|
+  g = Group.create title: "title-#{i}", description: "desc-#{i}", user_id: u.id
+  g.members << u
+  
+  30.times do |n|
+    Post.create content: "post-content-grp#{i}-post#{n}", group_id: g.id, user_id: u.id
+  end
+end
